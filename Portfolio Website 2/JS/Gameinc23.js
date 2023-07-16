1e51ecb015a6f39a8de796a050886d884308c6bc
AfifDanish150 <139652294+AfifDanish150@users.noreply.github.com>
2023-07-17 05:52:44 +0800

    Delete Portfolio Website 2 directory

--- Portfolio Website 2/JS/Gameinc23.js
+++
@@ -1,9 +0,0 @@
-// Get the image and box elements
-const image = document.querySelector('.image');
-const box = document.querySelector('.box');
-
-// Add a click event listener to the image
-image.addEventListener('click', function() {
-  // Toggle the 'active' class on the box
-  box.classList.toggle('active');
-});
